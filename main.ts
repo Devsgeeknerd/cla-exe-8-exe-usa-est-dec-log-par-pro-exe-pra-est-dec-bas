3f377de6b546ab802d97e8b6124e0c161dbc23bf
Paulo <vanderley.1109@gmail.com>
2024-01-16 11:16:38 -0300

    adicionando conteudo

---
+++ main.ts
@@ -0,0 +1,13 @@
+let letraUsuario: string = "a";
+
+if (
+  letraUsuario == "a" ||
+  letraUsuario == "e" ||
+  letraUsuario == "i" ||
+  letraUsuario == "o" ||
+  letraUsuario == "u"
+) {
+  console.log("Sua letra é uma vogal!");
+} else {
+  console.log("Sua letra é uma consoante!");
+}
