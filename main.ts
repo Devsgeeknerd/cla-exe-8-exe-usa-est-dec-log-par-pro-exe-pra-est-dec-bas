7b3992a37ab281b9a84cbed9b308d5f168c8e485
Paulo Vanderley <vanderley.1109@gmail.com>
2024-02-15 15:32:13 -0300

    atualizando conteudo

--- main.ts
+++ main.ts
@@ -1,5 +1,7 @@
+// Declaração de uma variável 'letraUsuario' do tipo string com valor 'a'.
 let letraUsuario: string = "a";
 
+// Verifica se a 'letraUsuario' é igual a uma das vogais: 'a', 'e', 'i', 'o' ou 'u'.
 if (
   letraUsuario == "a" ||
   letraUsuario == "e" ||
@@ -7,7 +9,9 @@ if (
   letraUsuario == "o" ||
   letraUsuario == "u"
 ) {
+  // Se a condição for verdadeira, imprime uma mensagem indicando que a letra é uma vogal.
   console.log("Sua letra é uma vogal!");
 } else {
+  // Se a condição for falsa, imprime uma mensagem indicando que a letra é uma consoante.
   console.log("Sua letra é uma consoante!");
 }
